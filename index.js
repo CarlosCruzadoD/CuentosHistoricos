6d303dd5e9dddd0d01976494d886d6c00d5ebafb
carloscd <carlos17cruzado@gmail.com>
2024-10-24 02:40:18 -0500

    Mejora de la pagina inicio

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-let titulo1 = document.getElementById("titulo1").innerHTML;
-let desc;
-let cat;
-let cuento;
+//----------------------------------------------------------------------
+//Funcion de las Letras de Bienvenida
+const spans = document.querySelectorAll('.word span');
 
-// Usamos fetch para obtener el contenido del archivo JSON
-fetch('prueba.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error al cargar el archivo JSON');
-    }
-    return response.json(); // Convertimos la respuesta a JSON
-  })
-  .then(data => {
-    //console.log(data);
-    data.forEach(element => {       
-        console.log(element.Cuento); 
-        if(element.titulo == titulo1){
-            desc = element.Descripcio;
-            cat = element.Categoras;
-            cuento = element.Cuento;
-        }
-    });
-  })
-  .catch(error => {
-    console.error('Hubo un problema:', error);
-  });
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
+//----------------------------------------------------------------------
+let currentSection = 0;
+const sections = document.querySelectorAll('section');
 
-  console.log(desc);
-  console.log(cat);
-  console.log(cuento);
+function scrollToSection(index) {
+  if (index >= 0 && index < sections.length) {
+    sections[index].scrollIntoView({ behavior: 'smooth' });
+    currentSection = index;
+  }
+}
+
+document.addEventListener('wheel', (event) => {
+  if (event.deltaY > 0) {
+    scrollToSection(currentSection + 1);
+  } else {
+    scrollToSection(currentSection - 1);
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    scrollToSection(currentSection + 1);
+  }
+});
+
+//----------------------------------------------------------------------
+const header = document.getElementById('header');
+const stickyOffset = header.offsetTop; // Obtiene la posición del header
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 120) {
+    header.classList.add('fixed'); // Agrega la clase cuando se desplaza
+  } else {
+    header.classList.remove('fixed'); // Remueve la clase si vuelve arriba
+  }
+});
