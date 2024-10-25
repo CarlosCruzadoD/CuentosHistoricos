4104ce329a766b3116cd6651336a74c3bc89b3d0
carloscd <carlos17cruzado@gmail.com>
2024-10-25 01:28:05 -0500

    Mejoras al header

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 //----------------------------------------------------------------------
+//Funcion para Login
+let perfil = true;
+if(perfil){
+  const iconPerfil = document.querySelector('.icon-perfil');
+  iconPerfil.style.display = 'block'
+}
+
+
+//----------------------------------------------------------------------
 //Funcion de las Letras de Bienvenida
 const spans = document.querySelectorAll('.word span');
 
@@ -16,7 +25,7 @@ spans.forEach((span, idx) => {
 	}, 750 * (idx+1))
 });
 //----------------------------------------------------------------------
-let currentSection = 0;
+let currentSection = 1;
 const sections = document.querySelectorAll('section');
 
 function scrollToSection(index) {
@@ -46,8 +55,35 @@ const stickyOffset = header.offsetTop; // Obtiene la posición del header
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 120) {
-    header.classList.add('fixed'); // Agrega la clase cuando se desplaza
+    header.classList.add('fixded'); // Agrega la clase cuando se desplaza
   } else {
-    header.classList.remove('fixed'); // Remueve la clase si vuelve arriba
+    header.classList.remove('fdixed'); // Remueve la clase si vuelve arriba
   }
 });
+
+
+//---------------------------------------------------------------------------
+const sectionsHeader = document.querySelectorAll('section');
+const btnHeader = document.getElementById('btn_act');
+const btnSecHeader = document.querySelectorAll('.link-sec');
+const listaPrimero = document.querySelectorAll('.lista-actual')
+
+let marcarSeccionActiva = () => {
+  let indexSeccionActiva = sections.length;
+  sectionsHeader.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= 50 && rect.bottom >= 50) {
+      indexSeccionActiva = index;
+      btnSecHeader.forEach((boton) => {
+        if(boton.innerHTML == sectionsHeader[indexSeccionActiva].id){
+          boton.innerHTML = btnHeader.innerHTML
+        }
+      })
+      btnHeader.innerHTML = sectionsHeader[indexSeccionActiva].id;
+      listaPrimero[0].classList.remove('lista-actual-animacion')
+      void listaPrimero[0].offsetWidth;
+      listaPrimero[0].classList.add('lista-actual-animacion')
+    }
+  })
+}
+window.addEventListener('scroll', marcarSeccionActiva);
